12869495d657a368fc04185cf1b75355acade089
tomek <sordontomek@gmail.com>
2020-03-22 16:53:13 +0100

    Admin Game CRUD

--- polls/models.py
+++ polls/models.py
@@ -6,19 +6,14 @@ from django.utils import timezone
 
 class Game(models.Model):
     name = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.game_text
-
+    @property
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class ScrapPattern(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
     table = models.CharField(max_length=200)
     archvement = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.ScrapPattern_text
