7bbd1ebd35696b122c0a03194cbb56d05e167f33
Xarv <strzelecki20@gmail.com>
2020-04-24 12:03:58 +0200

    WIP: get

--- games/urls.py
+++ games/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('games/', views.games, name='games'),
-    path('games/achievement/', views.achievement, name='achievement'),
+    path('games/<int:appid>/achievement/', views.achievement, name='achievement'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('error404', views.error404, name='error404'),
--- games/views.py
+++ games/views.py
@@ -4,7 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from games.forms import SendUrlForm
+from allauth.socialaccount.models import SocialAccount
+import environ
 
+
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '.env'))
 
 def home(request):
     return render(request, 'pages/home.html', {})
@@ -12,11 +17,26 @@ def home(request):
 
 @login_required(login_url='/account/steam/login')
 def games(request):
-    return render(request, 'pages/games.html', {})
+    for account in SocialAccount.objects.filter(user=request.user):
+        provider_account = account.get_provider_account().account.extra_data
+    api_base = "http://api.steampowered.com/"
+    method = "IPlayerService/GetOwnedGames/v0001/"
+    params = {"key": env("STEAM_KEY"), "steamid": provider_account["steamid"], "format":"json", "include_appinfo":"1"}
+    resp = requests.get(api_base + method, params)
+    data = resp.json()
+    method = "ISteamUserStats/GetSchemaForGame/v2/"
+    games = data["response"]["games"]
+    gameList = {}
+    for game in games:
+        params = {"key": env("STEAM_KEY"), "appid": game["appid"], "format": "json"}
+        resp = requests.get(api_base + method, params)
+        data = resp.json()
+        gameList[game["appid"]] = data["game"]
+    return render(request, 'pages/games.html', {"games": data["response"]})
 
 
 @login_required(login_url='/account/steam/login')
-def achievement(request):
+def achievement(request, appid):
     if request.method == 'POST':
         form = SendUrlForm(request.POST)
         if form.is_valid():
