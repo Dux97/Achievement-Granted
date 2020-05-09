ae04e9a42b27c996ff8f22c3d6222ca53d74c56b
Xarv <strzelecki20@gmail.com>
2020-05-09 20:25:54 +0200

    Refactor

--- games/views.py
+++ games/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from requests import ConnectionError, Timeout
 
 from games.forms import SendUrlForm
-from games.utils import scrapTableFromUrl, getUserGames, getGameInfo, getUnlockedAchievment, \
-    addUnlockedToDetails, addWikiLink,scrapLinkAndAddToTable
+from games.utils import getUserGames, getGameInfo, getUnlockedAchievment, \
+    addUnlockedToDetails, scrapLinkAndAddToTable
 
 
 def home(request):
@@ -28,34 +28,26 @@ def achievement(request, appid):
         data = {'form': form}
     else:
         form = SendUrlForm(request.POST)
+        errorDescr = {"errorDescription": False}
         if form.is_valid():
             url = request.POST['url']
             try:
-                gameScrapped ={} #scrapTableFromUrl(url)
-
                 game = getGameInfo(appid)
                 unlockedAchievement = getUnlockedAchievment(request, appid)
                 try:
                     steamAchievements = game['game']['availableGameStats']['achievements']
-                    gamedata2=scrapLinkAndAddToTable(url, steamAchievements)
-
+                    scrapedAchievements = scrapLinkAndAddToTable(url, steamAchievements)
                 except KeyError:
-                    data = {"errorDescription": "No achievements in this game."}
-                else:
-                    steamAchievements = game['game']['availableGameStats']['achievements']
-                    # gameAchievements = addWikiLink(steamAchievements, gameScrapped)
+                    errorDescr = {"errorDescription": "No achievements in this game."}
                 try:
                     playerUnlocked = [achievement['name'] for achievement in
                                       unlockedAchievement['playerstats']['achievements']]
                 except KeyError:
                     playerUnlocked = []
             except (ConnectionError, Timeout):
-                data = {'errorDescription': "Something wrong. Please try again."}
-            try:
-                data
-            except UnboundLocalError:
-                data = {'achievementSteam': addUnlockedToDetails(gamedata2, playerUnlocked),
-                        'achievementScrap': gameScrapped}
+                errorDescr = {'errorDescription': "Something wrong. Please try again."}
+            data = {'achievementSteam': addUnlockedToDetails(scrapedAchievements, playerUnlocked),
+                    "error": errorDescr}
     return render(request, 'pages/achievement.html', data)
 
 
